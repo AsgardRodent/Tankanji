d9ebb071ee528909d22dc5a2fd76d7d5d0eb7f15
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2024-12-17 03:21:16 -0800

    Fix: Remove unused imports and ESLint warnings

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import N5 from './components/n5/n5.tsx'
 import N4 from './components/n4/n4.tsx'
 import N3 from './components/n3/n3.tsx'
 import Home from './components/home/home.tsx'
-import Quiz from './components/quiz/quiz.tsx'
+//import Quiz from './components/quiz/quiz.tsx'
 // import Landing from './components/landing.jsx'
 
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/N5" element={<N5 />} />
         <Route path='/N4' element={<N4 />}/>
         <Route path='/N3' element = {<N3 />} /> 
-        <Route path="/Quiz" element={<Quiz />}/>
+        {/* <Route path="/Quiz" element={<Quiz />}/> */}
       </Routes>
     </Router>
   );
